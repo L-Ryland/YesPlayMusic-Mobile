e565ead5b2d044e9529bb5ca666af1cf9b298f74
L-Ryland <saintleso@gmail.com>
2022-02-02 16:04:03 +0800

    prebuild version

--- screens/LibraryScreen.tsx
+++ screens/LibraryScreen.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
+// import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, ScrollView } from "../components/Themed";
 import ConnectBackend from "../components/ConnectBackend";
 
