b5199674eac66d615afeebcc4a0e5a9b5f959ef1
L-Ryland <saintleso@gmail.com>
2022-02-02 22:34:41 +0800

    before implant nodejs-mobile

--- components/ConnectBackend.tsx
+++ components/ConnectBackend.tsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import { Text, Button } from "react-native";
-import nodejs from "nodejs-mobile-react-native";
+import { banner } from '../NeteaseCloudMusicApi'
 
-export default class ConnectBackend extends Component {
-  componentDidMount(){
-    nodejs.start('./NetaeseCloudMusicApi/app.js');
-    nodejs.channel.addListener('message',
-    (msg) => {
-      alert('Received from node: ' + msg);
-    },
-    this
-  );
-  };
 
-  render() {
-    return <Button title="Loading Backend" onPress={()=>{nodejs.channel.send('something')}}/>;
-  }
+
+export default async() => {
+  const result = await banner({ type: 0 }).then((res) => {
+    console.log(res)
+  })
+  console.log(result);
+  
+  return <Text>test</Text>;
 }
-
