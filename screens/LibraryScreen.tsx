cecc7adf2473c6888fe16668271337a53981d4f1
lianlian <kuliantnt@gmail.com>
2022-04-04 14:46:56 +0800

    push

--- screens/LibraryScreen.tsx
+++ screens/LibraryScreen.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
 import { fetchLikedThings, fetchUserProfile, selectData } from "@/redux/slice/dataSlice";
@@ -12,7 +13,6 @@ import { Text, View } from "../components/Themed";
 import Login from "./LoginScreen";
 import styled from "styled-components/native";
 import { database } from "@/index";
-import { Row } from "react-native-settings-screen/dist/lib/Row";
 
 const { width } = Dimensions.get('window')
 export const LibraryScreen = () => {
@@ -20,6 +20,7 @@ export const LibraryScreen = () => {
   const { user, liked } = data;
   const dispatch = useAppDispatch();
   const svgStyle = useSvgStyle({});
+  const [likedSongs, setLikedSongs] = React.useState<object[]>();
   const handleThunkTest = () => {
     dispatch(fetchUserProfile()).then(
       (action) => {
@@ -42,7 +43,7 @@ export const LibraryScreen = () => {
   console.log("liked", liked);
   const DATA = [0, 1, 2, 3, 4, 5]
 
-  const likedSongs = ({ item }) => (
+  const LikedSongs = ({ item }) => (
     <View style={styles.card2}>
       <Image
         style={styles.tinyLogo}
@@ -54,16 +55,14 @@ export const LibraryScreen = () => {
       </View>
     </View>
   );
-  const likedPlaylists = ({ item }) => (
+  const LikedPlaylists = ({ item }) => (
     <View style={styles.card3}>
       <Image
         style={styles.tinyLogo2}
         source={{ uri: user.backgroundUrl }}
       />
       <View style={styles.titleBox}>
-        {/* <Text style={styles.titleBig}>{"aaa"}</Text> */}
         <CoverTitle>{'aaa'}</CoverTitle>
-        {/* <Text style={styles.titleSmail}>{"aaa"}</Text> */}
         <CoverSubTitle>{'aaa'}</CoverSubTitle>
       </View>
     </View>
@@ -75,7 +74,7 @@ export const LibraryScreen = () => {
       </RowView>
       <Button children='userProfile' onPress={handleThunkTest} />
       <Button children='delete test database' onPress={handleDatabase} />
-      <ScrollView>
+      <ScrollView >
         <RowView >
           <ImageBackground
             source={{ uri: user.backgroundUrl }}
@@ -85,17 +84,15 @@ export const LibraryScreen = () => {
             blurRadius={10}
           >
             <Text style={styles.leftTitle}>{t('library.likedSongs')}</Text>
-            <RowView style={{ backgroundColor: 'transparent', flex: 1, margin: 15, justifyContent: 'flex-start' }}>
-              <Text style={{ fontSize: 20, flex: 1 }}>{t('common.songs')}</Text>
-            </RowView>
-            <RowView style={{ backgroundColor: 'transparent', flex: 3, marginRight: 20, justifyContent: 'flex-end' }}>
+            <Text style={styles.leftTitleSmail} numberOfLines={2}>{liked.songs.length} {t('common.songs')}</Text>
+            <RowView style={{ backgroundColor: 'transparent', flex:1.5, marginBottom: 20, marginRight: 20, justifyContent: 'flex-end' }}>
               <Play {...svgStyle} color='black' height={45} width={45} />
             </RowView>
 
           </ImageBackground>
           <FlatList style={{ flex: 1 }}
             data={DATA.slice(0, 3)}
-            renderItem={likedSongs}
+            renderItem={LikedSongs}
             keyExtractor={(item, index) => index.toString()}
             initialNumToRender={3} />
         </RowView>
@@ -103,15 +100,15 @@ export const LibraryScreen = () => {
           <Button>全部歌单</Button>
           <Button>专辑</Button>
         </RowView>
-        <RowView style={{ justifyContent: 'center' }}>
+        <View style={{ marginHorizontal: 20 }}>
           <FlatList
             data={DATA}
             numColumns={2}
-            renderItem={likedPlaylists}
+            renderItem={LikedPlaylists}
             keyExtractor={(item, index) => index.toString()}
             initialNumToRender={3}
-             />
-        </RowView>
+          />
+        </View>
       </ScrollView>
 
 
@@ -149,9 +146,9 @@ const styles = StyleSheet.create({
     flex: 1,
     borderRadius: 100,
     justifyContent: 'center',
-    height: 1 / 2 * width,
-    marginLeft: 8,
-    marginRight: 10
+    height: 1 / 2 * width - 20,
+    marginRight: 20,
+    marginLeft: 20
   },
   backgroundStyle: {
     borderRadius: 22,
@@ -167,19 +164,22 @@ const styles = StyleSheet.create({
   card2: {
     flex: 1,
     flexDirection: 'row',
-    marginTop: 20,
+    marginTop: 10,
     alignItems: 'center',
   },
   card3: {
-    flex: 1,
-    flexDirection: 'column',
-    marginTop: 20,
-    alignItems: 'center',
+    marginVertical: 20
   },
   leftTitle: {
-    fontSize: 27,
+    fontSize: 25,
     flex: 2,
-    margin: 10
+    marginTop: 10,
+    marginHorizontal: 10
+  },
+  leftTitleSmail: {
+    fontSize: 18,
+    flex: 2,
+    marginHorizontal: 10
   },
   tinyLogo: {
     borderRadius: 7,
@@ -189,9 +189,8 @@ const styles = StyleSheet.create({
   },
   tinyLogo2: {
     borderRadius: 19,
-    width: 1 / 2 * width - 50,
-    height: 1 / 2 * width - 50,
-    marginTop: 5
+    width: 1 / 2 * width - 30,
+    height: 1 / 2 * width - 30,
   },
   titleBig: {
     fontSize: 15,
@@ -205,11 +204,7 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 28,
   },
-  leftTitleSmail: {
-    margin: 5,
-    flex: 1,
-    fontSize: 18,
-  },
+
   titleBox: {
     width: 1 / 2 * width - 10
   },
